dc7d7e8897e49c9cc3d1520ff42fce220b5ec78b
Shawn Neal <sneal@sneal.net>
2014-03-08 07:29:48 -0800

    Added minitest

---
+++ files/default/tests/minitest/default_spec.rb
@@ -0,0 +1,10 @@
+require 'minitest/spec'
+require 'mixlib/shellout'
+
+class TestBeyondCompareInstall < MiniTest::Chef::Spec
+  describe 'install BC3' do
+    it 'should install bc3.exe' do
+      assert File.exists?(node['beyondcompare']['bcompare_exe'])
+    end
+  end
+end
