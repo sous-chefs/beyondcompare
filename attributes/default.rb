1210189a56f69b6a0f762780460001f0a15824ee
Shawn Neal <sneal@sneal.net>
2014-03-08 07:29:40 -0800

    Upgraded to BC3 3.3.9

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,13 @@
-default['beyondcompare']['version'] = '3.3.7.15876'
+default['beyondcompare']['version'] = '3.3.9.17611'
 default['beyondcompare']['base_url'] = 'http://www.scootersoftware.com'
 
-default['beyondcompare']['3.3.7.15876']['package_name'] = 'Beyond Compare Version 3.3.7'
-default['beyondcompare']['3.3.7.15876']['checksum'] =
-  '92b6970649fc519e535b304f762d40333344dd32d724e05979efc6adb48de45f'
+default['beyondcompare']['3.3.9.17611']['package_name'] = 'Beyond Compare Version 3.3.9'
+default['beyondcompare']['3.3.9.17611']['checksum'] =
+  'f22f6845a09124c5eb7e3c0cf995324757e6f0eac251a97ab5027bad020d342b'
 
+# Assume 64bit Windows
 default['beyondcompare']['bcompare_exe'] =
-  "#{ENV['ProgramFiles(x86)']}\\Beyond Compare 3\\BCompare.exe"
+  ::File.join('c:', 'Program Files (x86)', 'Beyond Compare 3', 'BCompare.exe')
 
 default['beyondcompare']['git_exe'] =
-  "#{ENV['ProgramFiles(x86)']}\\Git\\bin\\git.exe"
+  ::File.join('c:', 'Program Files (x86)', 'Git', 'bin', 'git.exe')
