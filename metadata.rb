a420a51d09fc9f0b25da8769a9667b5b8ce00a79
Linus Ruth <linus.ruth@changepoint.com>
2018-05-03 16:25:20 -0700

    chore: fix errors identified by foodcritic

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sneal@sneal.net'
 source_url       'https://github.com/windowschefcookbooks/beyondcompare'
 issues_url       'https://github.com/windowschefcookbooks/beyondcompare/issues'
 chef_version     '>= 12.6' if respond_to?(:chef_version)
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures Beyond Compare'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
