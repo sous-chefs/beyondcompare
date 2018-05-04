0575a10020db26f05c5c7dfdd8f405bdf9be7f34
Linus Ruth <linus.ruth@changepoint.com>
2018-05-04 13:25:40 -0700

    fix: version of beyond compare to match in install spec

--- spec/install_spec.rb
+++ spec/install_spec.rb
@@ -4,7 +4,7 @@ describe 'beyondcompare::install' do
     end.converge(described_recipe)
   end
   it 'installs Beyond Compare' do
-    expect(chef_run).to install_windows_package('Beyond Compare 4.2.4.22795')
+    expect(chef_run).to install_windows_package('Beyond Compare 4.2.4')
       .with(
         source: 'http://www.scootersoftware.com/BCompare-4.2.4.22795.exe',
         checksum: 'b4b2bed8e5fe16a8680ffb4f3827a892cbfecc58803cc59c70bc66e60a92e291',
